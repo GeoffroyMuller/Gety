27543ebeccf6b9c658f20bdfefe628f16af78dd0
etienne1698 <etienne.robert1698@gmail.com>
2020-06-09 21:10:09 +0200

    init backend

---
+++ gety_back/bin/main.dart
@@ -0,0 +1,13 @@
+import 'package:gety_back/gety_back.dart';
+
+Future main() async {
+  final app = Application<GetyBackChannel>()
+      ..options.configurationFilePath = "config.yaml"
+      ..options.port = 8888;
+
+  final count = Platform.numberOfProcessors ~/ 2;
+  await app.start(numberOfInstances: count > 0 ? count : 1);
+
+  print("Application started on port: ${app.options.port}.");
+  print("Use Ctrl-C (SIGINT) to stop running the application.");
+}
---
+++ gety_back/lib/gety_back.dart
@@ -0,0 +1,11 @@
+/// gety_back
+///
+/// A Aqueduct web server.
+library gety_back;
+
+export 'dart:async';
+export 'dart:io';
+
+export 'package:aqueduct/aqueduct.dart';
+
+export 'channel.dart';
