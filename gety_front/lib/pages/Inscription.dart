5ea5129ca87b2e7fd93bb926aad97fc90a71c65a
geoffroy <geoffroym2@gmail.com>
2020-06-11 22:33:22 +0200

    form

--- gety_front/lib/pages/Inscription.dart
+++ gety_front/lib/pages/Inscription.dart
@@ -12,67 +12,33 @@ class Inscription extends StatefulWidget {
 class _Inscription extends State<Inscription> {
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: const <Widget>[
-        Card(child: ListTile(title: Text('One-line ListTile'))),
-        Card(
-          child: ListTile(
-            leading: FlutterLogo(),
-            title: Text('One-line with leading widget'),
+    return new Form(
+        child: Padding(
+      padding: new EdgeInsets.all(20),
+      child: new Column(
+        children: <Widget>[
+          TextField(
+            decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'email'
+            ),
           ),
-        ),
-        Card(
-          child: TextField(
-            obscureText: false,
+          SizedBox(height: 16.0),
+          TextField(
             decoration: InputDecoration(
               border: OutlineInputBorder(),
-              labelText: 'Password',
+              labelText: 'mdp'
             ),
+          ),
+          Row(
+            children: <Widget>[
+              OutlineButton(
+                child: Text('Connexion'),
+              ),
+            ],
           )
-        ),
-        Card(
-          child: ListTile(
-            leading: FlutterLogo(),
-            title: Text('One-line with both widgets'),
-            trailing: Icon(Icons.more_vert),
-          ),
-        ),
-        Card(
-          child: FlatButton(
-            color: Colors.blue,
-            textColor: Colors.white,
-            disabledColor: Colors.grey,
-            disabledTextColor: Colors.black,
-            padding: EdgeInsets.all(8.0),
-            splashColor: Colors.blueAccent,
-            child: Text(
-              "Flat Button",
-              style: TextStyle(fontSize: 20.0),
-            ),
-          )
-        ),
-        Card(
-          child: ListTile(
-            leading: FlutterLogo(size: 56.0),
-            title: Text('Etienne a trouvé lamour'),
-            subtitle: Text('nique etienne'),
-            trailing: Icon(Icons.more_vert),
-          ),
-        ),
-        Card(
-          child: ListTile(
-            leading: FlutterLogo(size: 72.0),
-            title: Text('Three-line ListTile'),
-            subtitle: Text(
-                'A sufficiently long subtitle warrants three lines.'
-            ),
-            trailing: Icon(Icons.more_vert),
-            isThreeLine: true,
-          ),
-        ),
-      ],
-
-
-    );
+        ],
+      ),
+    ));
   }
 }
