846c320554994869fd8c26a5f5dfb3a658b8f9de
geoffroy <geoffroym2@gmail.com>
2020-06-11 20:57:17 +0200

    init front

---
+++ gety_front/lib/models/User.dart
@@ -0,0 +1,20 @@
+
+class User {
+
+  int id;
+
+  String avatar;
+
+  String firstname;
+
+  String lastname;
+
+  String email;
+
+  String password;
+
+  String toString() {
+    return 'User{id: $id, avatar: $avatar, firstname: $firstname, lastname: $lastname, email: $email}';
+  }
+  
+}
