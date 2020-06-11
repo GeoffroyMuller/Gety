6406e2530c5031d43c1dfa8ff79dd348b4acf062
geoffroy <geoffroym2@gmail.com>
2020-06-11 21:49:45 +0200

    etienne

--- gety_front/lib/pages/Inscription.dart
+++ gety_front/lib/pages/Inscription.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class Inscription extends StatefulWidget {
@@ -11,15 +13,65 @@ class _Inscription extends State<Inscription> {
   @override
   Widget build(BuildContext context) {
     return ListView(
-        children: const <Widget>[
-          TextField(
-            obscureText: true,
+      children: const <Widget>[
+        Card(child: ListTile(title: Text('One-line ListTile'))),
+        Card(
+          child: ListTile(
+            leading: FlutterLogo(),
+            title: Text('One-line with leading widget'),
+          ),
+        ),
+        Card(
+          child: TextField(
+            obscureText: false,
             decoration: InputDecoration(
               border: OutlineInputBorder(),
               labelText: 'Password',
             ),
           )
-        ],
+        ),
+        Card(
+          child: ListTile(
+            leading: FlutterLogo(),
+            title: Text('One-line with both widgets'),
+            trailing: Icon(Icons.more_vert),
+          ),
+        ),
+        Card(
+          child: FlatButton(
+            color: Colors.blue,
+            textColor: Colors.white,
+            disabledColor: Colors.grey,
+            disabledTextColor: Colors.black,
+            padding: EdgeInsets.all(8.0),
+            splashColor: Colors.blueAccent,
+            child: Text(
+              "Flat Button",
+              style: TextStyle(fontSize: 20.0),
+            ),
+          )
+        ),
+        Card(
+          child: ListTile(
+            leading: FlutterLogo(size: 56.0),
+            title: Text('Etienne a trouvé lamour'),
+            subtitle: Text('nique etienne'),
+            trailing: Icon(Icons.more_vert),
+          ),
+        ),
+        Card(
+          child: ListTile(
+            leading: FlutterLogo(size: 72.0),
+            title: Text('Three-line ListTile'),
+            subtitle: Text(
+                'A sufficiently long subtitle warrants three lines.'
+            ),
+            trailing: Icon(Icons.more_vert),
+            isThreeLine: true,
+          ),
+        ),
+      ],
+
 
     );
   }
