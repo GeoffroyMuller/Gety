4128a7dd27523fe0a8a1626e558ce3a0444f8fe3
etienne1698 <etienne.robert1698@gmail.com>
2020-06-15 21:08:00 +0200

    up

--- gety_front/lib/services/AbstractService.dart
+++ gety_front/lib/services/AbstractService.dart
@@ -1,15 +1,10 @@
+import 'package:gety_front/config/parameters.dart';
 import 'package:gety_front/models/User.dart';
 import 'package:http/http.dart' as http;
 
 
-// todo : replace this in config file
-const apiUrl = "http://localhost:8888/";
-
-
 abstract class AbstractService<T> {
   String url = apiUrl;
-
-
 
 
 }
