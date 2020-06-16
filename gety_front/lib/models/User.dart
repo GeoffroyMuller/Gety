3e454786ff3307c6e2328fbc955a60041dc3ecb3
etienne1698 <etienne.robert1698@gmail.com>
2020-06-16 21:41:33 +0200

    up

--- gety_front/lib/models/User.dart
+++ gety_front/lib/models/User.dart
@@ -15,8 +15,16 @@ class User {
     return 'User{id: $id, avatar: $avatar, firstname: $firstname, lastname: $lastname, email: $email}';
   }
 
-  User.fromJson(Map<String, dynamic> json):
-        id = json['id'],
+  User(
+      {this.id = 0,
+      this.avatar = '',
+      this.firstname = '',
+      this.lastname = '',
+      this.email = '',
+      this.password = ''});
+
+  User.fromJson(Map<String, dynamic> json)
+      : id = json['id'],
         firstname = json['firstname'],
         lastname = json['lastname'],
         email = json['email'],
--- gety_front/lib/services/UserService.dart
+++ gety_front/lib/services/UserService.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
+
 import 'package:gety_front/models/User.dart';
 import 'package:gety_front/services/AbstractService.dart';
 import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
 
 class UserService extends AbstractService {
   static UserService _instance;
@@ -9,10 +12,20 @@ class UserService extends AbstractService {
     url += "users";
   }
 
-  static UserService get getInstance =>
+  static UserService get instance =>
       _instance = _instance ?? UserService._();
 
+  /*
   Future<List<User>> getUsers() async {
     var response = await http.get(url);
+    print(response);
+  }
+   */
+  
+  Future register(User user) async {
+    print(url);
+    print(user);
+    http.Response response = await http.post(url, body: user.toString());
+    print(User.fromJson(jsonDecode(response.toString())));
   }
 }
