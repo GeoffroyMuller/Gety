8e7c6d01faae31a257337e1c1455f16213002048
etienne1698 <etienne.robert1698@gmail.com>
2020-06-18 19:25:31 +0200

    up

--- gety_back/lib/route.dart
+++ gety_back/lib/route.dart
@@ -1,11 +1,11 @@
 import 'dart:io';
-import '../core/Controller.dart';
 import '../core/Router.dart';
 
 Future<void> route(Router router) async {
+  router.get("test", test);
+}
 
-  router.get("test", (HttpRequest request) => {
-      request.response.write("TEST")
-  });
-
+void test(HttpRequest request) {
+  print("TEST URL CALL");
+  request.response.write("TEST");
 }
