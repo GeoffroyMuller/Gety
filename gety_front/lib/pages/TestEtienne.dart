353dd36a2b44ac7e5677fd7e53dc6deadd0b7209
etienne1698 <etienne.robert1698@gmail.com>
2020-06-30 00:22:31 +0200

    ADD : modal delete users

--- gety_front/lib/pages/TestEtienne.dart
+++ gety_front/lib/pages/TestEtienne.dart
@@ -15,8 +15,20 @@ class TestEtienne extends StatefulWidget {
 
 class _TestEtienne extends State<TestEtienne> {
   List<User> _users = [
-    new User(id: 1, firstname: "Etienne", lastname: "ROBERT", email: "etienne@localmail.fr", password: "testpwd", avatar: "etienne"),
-    new User(id: 2, firstname: "Geoffroy", lastname: "MULLER", email: "geoff@localmail.fr", password: "testpwd", avatar: "geoff")
+    new User(
+        id: 1,
+        firstname: "Etienne",
+        lastname: "ROBERT",
+        email: "etienne@localmail.fr",
+        password: "testpwd",
+        avatar: "etienne"),
+    new User(
+        id: 2,
+        firstname: "Geoffroy",
+        lastname: "MULLER",
+        email: "geoff@localmail.fr",
+        password: "testpwd",
+        avatar: "geoff")
   ];
 
   List<int> _selectedUser = [];
@@ -28,7 +40,6 @@ class _TestEtienne extends State<TestEtienne> {
       _selectedUser.add(user.id);
     }
     setState(() {});
-
   }
 
   @override
@@ -42,26 +53,27 @@ class _TestEtienne extends State<TestEtienne> {
         margin: EdgeInsets.only(left: 0.0, top: 20.0),
         child: ListView(
           children: <Widget>[
-            this.renderUserTable(),
+            this._renderUserTable(),
             Align(
                 alignment: Alignment(.925, 0),
                 child: IconButton(
-                    icon: Icon(Icons.delete_forever, color: Colors.red),
-                    onPressed: () => {this.delete()},
-                    //child: Text('Delete', style: TextStyle(color: Colors.black))
-                )
-            ),
+                  icon: Icon(Icons.delete_forever, color: Colors.red),
+                  onPressed: () => {this._showDeleteDialog()},
+                  //child: Text('Delete', style: TextStyle(color: Colors.black))
+                )),
           ],
         ),
       ),
     );
   }
 
-  delete() {
-    print(_selectedUser);
+  // todo : delete users
+  _delete({List<User> users}) {
+    print('users to delete');
+    print(users);
   }
 
-  renderUserTable() {
+  _renderUserTable() {
     return DataTable(
         columnSpacing: 4,
         showCheckboxColumn: true,
@@ -70,21 +82,64 @@ class _TestEtienne extends State<TestEtienne> {
         columns: const <DataColumn>[
           DataColumn(label: Text('ID', style: listHeaderTextStyle)),
           DataColumn(label: Text('Avatar', style: listHeaderTextStyle)),
-          DataColumn(label: Text('Firstname', style: listHeaderTextStyle)),
-          DataColumn(label: Text('Lastname', style: listHeaderTextStyle)),
+          DataColumn(label: Text('Name', style: listHeaderTextStyle)),
           DataColumn(label: Text('Email', style: listHeaderTextStyle)),
         ],
-        rows: _users.map((user) => DataRow(
-            onSelectChanged: (bool input)  => this.selectUser(user),
-            selected: _selectedUser.indexOf(user.id) != -1,
-            cells: <DataCell>[
-              DataCell(Text(user.id.toString())/*, onTap: ()  => this.selectUser(user)*/),
-              DataCell(Text(user.avatar), /*, onTap: ()  => this.selectUser(user)*/),
-              DataCell(Text(user.firstname)/*, onTap: ()  => this.selectUser(user)*/),
-              DataCell(Text(user.lastname)/*, onTap: ()  => this.selectUser(user)*/),
-              DataCell(Text(user.email)/*, onTap: ()  => this.selectUser(user)*/)
-            ]
-        )).toList()
+        rows: _users
+            .map((user) => DataRow(
+                    onSelectChanged: (bool input) => this.selectUser(user),
+                    selected: _selectedUser.indexOf(user.id) != -1,
+                    cells: <DataCell>[
+                      DataCell(Text(user.id
+                          .toString()) /*, onTap: ()  => this.selectUser(user)*/),
+                      DataCell(
+                        Text(user
+                            .avatar), /*, onTap: ()  => this.selectUser(user)*/
+                      ),
+                      DataCell(Text(user.firstname.substring(0, 1) +
+                          '.' +
+                          user.lastname
+                              .toUpperCase()) /*, onTap: ()  => this.selectUser(user)*/),
+                      DataCell(Text(
+                          user.email) /*, onTap: ()  => this.selectUser(user)*/)
+                    ]))
+            .toList());
+  }
+
+  Future<void> _showDeleteDialog() async {
+    List<User> users =
+        _users.where((u) => _selectedUser.contains(u.id)).toList();
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Delete users'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                    Text('Are you sure to delete :',
+                        style: TextStyle(color: Colors.red, height: 5)),
+                  ] + users.map((user) => Text(user.id.toString() + " - " + user.email)).toList(),
+            ),
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('Cancel'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            FlatButton(
+              child: Text('Confirm', style: TextStyle(color: Colors.red)),
+              onPressed: () {
+                _delete(users: users);
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
     );
   }
 }
